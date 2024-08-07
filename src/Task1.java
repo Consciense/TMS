798b12bda7f9c794d1fb63d1195cc08233d14dd1
Consciense <mr.jj921@gmail.com>
2024-08-07 20:58:59 +0300

    Deleting comments

--- src/Task1.java
+++ src/Task1.java
@@ -1,6 +1,3 @@
-/*    Задача 1:
-        Написать приложение, которое будет вычислять и выводить значение по формуле:
-        a=4*(b+c-1)/2; b и c задаем в коде самостоятельно  */
 public class Task1 {
      static double getTaskResult(double b, double c){
         return 4*(b+c-1)/2;
