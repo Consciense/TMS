484b7de84259cf17cf964ce73f6a9da8818e4fdc
Consciense <mr.jj921@gmail.com>
2024-08-21 17:05:08 +0300

    Added Dentist logic

---
+++ src/Dentist.java
@@ -0,0 +1,8 @@
+public class Dentist extends MedicalStaff{
+
+    @Override
+    public void treatPatient(Patient patient) {
+        System.out.println(getProfession() + " starts treatment of" + patient.getName() + "...");
+        System.out.println("Making some terrible stuff with teeth...");
+    }
+}
