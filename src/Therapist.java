ee228617b3ba0dc172cf0ba2034a82f2f398e59f
Consciense <mr.jj921@gmail.com>
2024-08-21 17:03:53 +0300

    Added Therapist logic

--- src/Therapist.java
+++ src/Therapist.java
@@ -1,18 +1,17 @@
 public class Therapist extends MedicalStaff {
 
-    @Override
-    public void treatPatient(Patient patient) {
-        super.treatPatient(patient);
-    }
-
     public void scheduleDoctor(Patient patient) {
         System.out.println("Therapist schedules a doctor for " + patient.getName());
         if (patient.getTreatmentPlan() == 1) {
+            System.out.println(patient.getName() + " is going to the surgeon.");
             patient.setTreatingDoctor("Surgeon");
+
         }
         if (patient.getTreatmentPlan() == 2) {
+            System.out.println(patient.getName() + " is going to the dentist.");
             patient.setTreatingDoctor("Dentist");
         } else {
+            System.out.println(patient.getName() + " is going to the therapist.");
             patient.setTreatingDoctor("Therapist");
             treatPatient(patient);
         }
