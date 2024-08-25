27625e420037b68a5562e361a90ba4c9adcb7568
Consciense <mr.jj921@gmail.com>
2024-08-25 13:21:25 +0300

    creating project

--- src/Dentist.java
+++
@@ -1,9 +0,0 @@
-public class Dentist extends MedicalStaff{
-
-    @Override
-    public void treatPatient(Patient patient) {
-        System.out.println(getProfession() + " starts treatment of " + patient.getName() + "...");
-        System.out.println("Making some terrible stuff with teeth...");
-        System.out.println("----------------------------------------------------------------");
-    }
-}
