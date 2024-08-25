0ba2c2eb872c8b2563ab20fecba0b6cd0cd4b74c
Consciense <mr.jj921@gmail.com>
2024-08-25 18:46:07 +0300

    Created task2 solution

---
+++ src/HiddenDog.java
@@ -0,0 +1,30 @@
+public class HiddenDog extends Animal {
+
+    private HiddenDog(String animalType) {
+        super(animalType);
+    }
+
+    @Override
+    public void voice(boolean isHappy) {
+        super.voice(isHappy);
+        System.out.println("Dog barks (this means that dog is glad to see you)" +
+                (isHappy ? "" : "\n Not your terrible food"));
+    }
+
+    @Override
+    public void eat(String food) {
+        super.eat(food);
+        System.out.println("Your food looks back...");
+        if (food.equals("Meat")) {
+            System.out.println("Dog starts to eat.");
+            voice(true);
+        } else {
+            System.out.println("Dog keeps staring at you wagging his tail.");
+            voice(false);
+        }
+    }
+
+    public static HiddenDog getHiddenDog() {
+        return new HiddenDog("Dog");
+    }
+}
